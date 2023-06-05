f788c4bd868c38c9189851fd213cc747f548ed34
SamawiaShahid <samawiashahid725@gmail.com>
2023-06-05 11:58:09 +0500

    File is completed.

---
+++ index.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let vowels = await inquirer.prompt([
+    {
+    name:  "character",
+    type:   "Input",
+    message:  "Please enter a character:",
+}
+]);
+let caseLower = vowels.character.toLowerCase();
+
+if(caseLower === 'a' || caseLower === 'e'  || caseLower === 'i'  || caseLower === 'o'  || caseLower === 'u'){
+    console.log(chalk.yellow(`"You entered the character '${caseLower}', which is a Vowel.`) );
+}else {
+    console.log(chalk.green(`You entered the character '${caseLower}', which is a Consonant.`) );
+}
